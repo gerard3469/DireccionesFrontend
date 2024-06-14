e142f2baf2a9ccf5a76d77309b2ece6b611524cb
gerard3469 <49664255+gerard3469@users.noreply.github.com>
2024-06-14 02:25:38 -0600

    Se agrega la lista, detalle, actualizción y creación de contactos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContactListComponent } from './components/contact-list/contact-list.component';
+import { ContactDetailComponent } from './components/contact-detail/contact-detail.component';
+import { ContactFormComponent } from './components/contact-form/contact-form.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'contactList', component: ContactListComponent },
+  { path: '', redirectTo: 'contactList', pathMatch: 'full' },
+  { path: 'contact/:id', component: ContactDetailComponent },
+  { path: 'edit/:id', component: ContactFormComponent },
+  { path: 'new', component: ContactFormComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,17 +3,30 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ContactListComponent } from './components/contact-list/contact-list.component';
+import { provideHttpClient, withFetch } from '@angular/common/http';
+import { ContactDetailComponent } from './components/contact-detail/contact-detail.component';
+import { ContactFormComponent } from './components/contact-form/contact-form.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NgxPaginationModule } from 'ngx-pagination';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactListComponent,
+    ContactDetailComponent,
+    ContactFormComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    NgxPaginationModule,
+    FormsModule
   ],
   providers: [
-    provideClientHydration()
+    provideClientHydration(),
+    provideHttpClient(),
   ],
   bootstrap: [AppComponent]
 })
